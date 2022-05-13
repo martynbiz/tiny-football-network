a3e530c02efdb045524a365b0fab68fbdc09bb9c
Martyn Bissett <martynbissett@yahoo.co.uk>
2022-05-13 17:57:56 +0100

    Register healthcheck

--- nakama/main.ts
+++ nakama/main.ts
@@ -1,4 +1,5 @@
 let InitModule: nkruntime.InitModule =
     function (ctx: nkruntime.Context, logger: nkruntime.Logger, nk: nkruntime.Nakama, initializer: nkruntime.Initializer) {
+        initializer.registerRpc('healthcheck', rpcHealthcheck)
         logger.info("Hello World!");
     }
